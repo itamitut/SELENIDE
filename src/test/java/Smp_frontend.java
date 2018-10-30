513a647bc7944dafd82d5e0376ec8a28f0ac240f
Sergey Avdeenkov <32090146+itamitut@users.noreply.github.com>
2018-10-30 16:51:27 +0300

    Селектор не дается

--- src/test/java/Smp_frontend.java
+++ src/test/java/Smp_frontend.java
@@ -3,9 +3,7 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -16,6 +14,10 @@ public class Smp_frontend {
         open("http://167.99.134.79/smp_frontend/login.html");
         $(byName("username")).setValue("Igor").pressEnter();
         $(byName("password")).setValue("helloworld").pressEnter();
-        
+        $(byLinkText("Локальная среда")).click();
+        $(byLinkText("Облачная среда")).click();
+
+        //        $(".select2-environment-container").selectOption("Облачная среда");
+        $(byLinkText("Вход")).click();
     }
 }
