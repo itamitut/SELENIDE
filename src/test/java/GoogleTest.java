d72f03ca7a42819c4b9adbe63a32207d069e6532
Sergey Avdeenkov <32090146+itamitut@users.noreply.github.com>
2019-01-23 16:45:56 +0300

    23/01

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,14 +1,25 @@
-import org.junit.Test;
+import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleTest {
+
+    @BeforeMethod
+    public static void setup() {
+        Configuration.baseUrl = "http://google.com";
+
+    }
+
     @Test
     public void user_can_search_everything_in_google() {
-        open("http://google.com/ncr");
+
+        open("/");
         $(By.name("q")).val("selenide").pressEnter();
 
         System.out.println("Количество найденных ссылок:" + $$("#ires .g").size());        //.shouldHave(size(7));
